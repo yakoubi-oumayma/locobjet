d062077a83d41206be41e6634f43de7730d448eb
Simofatt <100781782+Simofatt@users.noreply.github.com>
2023-04-10 01:49:37 +0200

    Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,27 +6,35 @@ use App\Models\AdReview;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
+
 
 
 class ProfileController extends Controller
 {
+
     public function index()
     {
-
+        if (!session()->has('email')) {
+            return view('SignIn');
+        } else {
             $user = new User();
             $user_infos = $user::selectUser();
+            $email_session = session('email');
             //$userId = auth()->user()->id; // Get the ID of the currently logged in user
-            $user_id = 1;
-            $reviews = DB::select("SELECT * FROM ad_reviews,ads,items,users WHERE
-                        ad_reviews.ad_id=ads.ad_id AND ads.item_id=items.item_id AND items.user_id=users.user_id AND users.user_id=?",[$user_id]);
-            /*$reviews = AdReview::whereHas('ad.item.user', function ($query) use ($user_id) {
+            $user_id = $user::getUserId();
+            /* $reviews = DB::select("SELECT * FROM ad_reviews,ads,items,users WHERE
+                        ad_reviews.ad_id=ads.ad_id AND ads.item_id=items.item_id AND items.user_id=users.user_id AND users.user_id=?",[$user_id]);*/
+            $reviews = AdReview::whereHas('ad.item.user', function ($query) use ($user_id) {
                 $query->where('user_id', $user_id);
-            })->get();*/
+            })->get();
 
             return view('Profile')->with([
                 'user_infos' => $user_infos,
-                'reviews'    => $reviews
+                'reviews'    => $reviews,
+                'email_session' => $email_session
             ]);
+        }
     }
 
     public function settings()
@@ -50,7 +58,11 @@ class ProfileController extends Controller
             "f_name" => "nullable",
             "l_name" => "nullable",
             "username" => "nullable",
-            "email" => "nullable|email|unique:users,email," . $user->id,
+            "email" => [
+                "nullable",
+                "email",
+                Rule::unique("users", "email")->ignore($user->user_id, "user_id")
+            ],
             "password" => "nullable"
         ]);
         $user->updateFields($request->all());
